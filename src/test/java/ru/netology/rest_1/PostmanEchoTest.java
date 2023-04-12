02284c0fca78de271fa40154a5a03e96b4612e80
komshuk <88716968+komshuk@users.noreply.github.com>
2023-04-13 00:30:50 +0300

    Update PostmanEchoTest.java

--- src/test/java/ru/netology/rest_1/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest_1/PostmanEchoTest.java
@@ -14,7 +14,7 @@ public class PostmanEchoTest {
                 .when()
                 .post("/post")
                 .then()
-                .statusCode(200)
+                .statusCode(500)
                 .body("data", equalTo("some data"))
         ;
     }
